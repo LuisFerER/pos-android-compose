7b3901bffee0cd8a57ab3e5e0a6c2616b2c3a866
LuisFerER <luisfercho123@live.com.mx>
2026-02-06 23:40:38 -0600

    Initial commit: Project setup with Compose and Landscape mode

---
+++ app/src/main/java/com/devsMarr/pos_galeriaemi/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.devsMarr.pos_galeriaemi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.devsMarr.pos_galeriaemi.ui.theme.PosGaleriaEmiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PosGaleriaEmiTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PosGaleriaEmiTheme {
+        Greeting("Android")
+    }
+}
